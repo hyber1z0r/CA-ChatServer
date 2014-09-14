b214073990e7317eeb68acb225c0d110c0263ac7
jakobgaardandersen <jakobgaardandersen@jakobs-mbp>
2014-09-14 12:49:34 +0200

    Fixed documentation, filen skal ligge i dist mappen

--- CA-ChatServer/src/Http/HttpServer.java
+++ CA-ChatServer/src/Http/HttpServer.java
@@ -22,17 +22,14 @@ import java.util.logging.Logger;
  *
  * @author Neno
  */
-public class HttpServer extends Thread
-{
+public class HttpServer extends Thread {
 
     static int port = 8080;
     static String ip = "100.85.76.5";
     static String contentFolder = "public/";
 
-    public static void main(String[] args) throws Exception
-    {
-        if (args.length == 3)
-        {
+    public static void main(String[] args) throws Exception {
+        if (args.length == 3) {
             port = Integer.parseInt(args[1]);
             ip = args[0];
             contentFolder = args[2];
@@ -42,31 +39,27 @@ public class HttpServer extends Thread
         server.createContext("/", new RequestHandler2());
         server.createContext("/bootstrap.min.css", new CSSRequestHandler());
         server.createContext("/Chatlog", new ChatlogRequestHandler());
-        server.createContext("/CA-ChatClient", new DownloadRequestHandler());
-        
+        server.createContext("/CA-ChatClient.jar", new DownloadRequestHandler());
+        server.createContext("/Documentation.pdf", new DocumentationRequestHandler());
+
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
     }
 
     @Override
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             main(null);
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logger.getLogger(HttpServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    static class RequestHandler implements HttpHandler
-    {
+    static class RequestHandler implements HttpHandler {
 
         @Override
-        public void handle(HttpExchange he) throws IOException
-        {
+        public void handle(HttpExchange he) throws IOException {
             String path = contentFolder + "onlineusers.html";
             File file = new File(path);
             Path path1 = Paths.get(path);
@@ -77,132 +70,112 @@ public class HttpServer extends Thread
             Files.write(path1, content.getBytes(charset));
 
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
             String contentType = "text/html";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
         }
 
-        
     }
-    
-     static class RequestHandler2 implements HttpHandler
-     {
+
+    static class RequestHandler2 implements HttpHandler {
 
         @Override
-        public void handle(HttpExchange he) throws IOException
-        {
+        public void handle(HttpExchange he) throws IOException {
             String path = contentFolder + "index.html";
             File file = new File(path);
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
             String contentType = "text/html";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
-        }        
-         
-     }
-     static class CSSRequestHandler implements HttpHandler
-     {
-         @Override
-         public void handle(HttpExchange he) throws IOException
-        {
+        }
+
+    }
+
+    static class CSSRequestHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
             String path = contentFolder + "bootstrap.min.css";
             File file = new File(path);
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
             String contentType = "text/css";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
-        }  
-     }
-     
-     
-     static class DownloadRequestHandler implements HttpHandler
-     {
-         @Override
-         public void handle(HttpExchange he) throws IOException
-        {
+        }
+    }
+
+    static class DownloadRequestHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
             String path = "dist/CA-ChatServer.jar";
             File file = new File(path);
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
             String contentType = "application/java-archive";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
-        }  
-     }
-     
-     static class ChatlogRequestHandler implements HttpHandler
-     {
+        }
+    }
+
+    static class ChatlogRequestHandler implements HttpHandler {
 
         @Override
-        public void handle(HttpExchange he) throws IOException
-        {
+        public void handle(HttpExchange he) throws IOException {
             String path = contentFolder + "Chatlog.html";
             File file = new File(path);
-            String ChatPath = "chatLog.txt"; 
+            String ChatPath = "chatLog.txt";
             Scanner scan = new Scanner(new File(ChatPath));
             StringBuilder sb = new StringBuilder();
-            while (scan.hasNext())
-            {
-              sb.append("<tr><td>");
-              sb.append(scan.nextLine());
-              sb.append("</td><td>");
-              sb.append(scan.nextLine());
-              sb.append("</td><tr>");
+            while (scan.hasNext()) {
+                sb.append("<tr><td>");
+                sb.append(scan.nextLine());
+                sb.append("</td><td>");
+                sb.append(scan.nextLine());
+                sb.append("</td><tr>");
             }
             sb.append("</tr>");
             Path path1 = Paths.get(path);
@@ -213,52 +186,45 @@ public class HttpServer extends Thread
             Files.write(path1, content.getBytes(charset));
             scan.close();
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
             String contentType = "text/html";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
-        }        
-         
-     }
-     
-        static class DocumentationRequestHandler implements HttpHandler
-     {
-         @Override
-         public void handle(HttpExchange he) throws IOException
-        {
+        }
+
+    }
+
+    static class DocumentationRequestHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
             String path = "dist/Documentation.pdf";
             File file = new File(path);
             byte[] bytesToSend = new byte[(int) file.length()];
-            try
-            {
+            try {
                 BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
                 bis.read(bytesToSend, 0, bytesToSend.length);
-            } catch (IOException ie)
-            {
+            } catch (IOException ie) {
                 ie.printStackTrace();
             }
-            String contentType = "application/java-archive";
+            String contentType = "application/pdf";
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", contentType);
             he.sendResponseHeaders(200, bytesToSend.length);
-            try (OutputStream os = he.getResponseBody())
-            {
+            try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
-                
+
             }
-        }  
-     }
+        }
+    }
 }
